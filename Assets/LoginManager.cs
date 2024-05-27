23c8eb7b6f831996d7fd5b6770df1b4a1ac8c1d5
FedericoGaubeca <47962250@est.ort.edu.ar>
2024-05-27 15:47:25 -0300

    Login Manager Script

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LoginManager : MonoBehaviour
+{
+    //public Text accesGrantedTxt;
+    //public Text accesDeniedTxt;
+    public Text inputText;
+
+    public string password;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    public void Acept()
+    {
+        if(inputText.text == password) //ElPepe123
+        {
+            Debug.Log("Acces Granted");
+        } else { Debug.Log("Acces Denied"); }
+    }
+}
